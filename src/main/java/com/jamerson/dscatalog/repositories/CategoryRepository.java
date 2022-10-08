8335eddeb73758645eafd333720b0047758ee9d2
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2022-10-08 16:16:48 -0300

    database seeding

---
+++ src/main/java/com/jamerson/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.jamerson.dscatalog.repositories;
+
+import com.jamerson.dscatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/jamerson/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.jamerson.dscatalog.services;
+
+import com.jamerson.dscatalog.entities.Category;
+import com.jamerson.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
