83a91741e24c22364d4e87bb4bc6f4d8e3de3ce7
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2022-10-08 17:27:56 -0300

    Error handling

--- src/main/java/com/jamerson/dscatalog/services/CategoryService.java
+++ src/main/java/com/jamerson/dscatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.jamerson.dscatalog.services;
 import com.jamerson.dscatalog.dto.CategoryDTO;
 import com.jamerson.dscatalog.entities.Category;
 import com.jamerson.dscatalog.repositories.CategoryRepository;
+import com.jamerson.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,6 +21,14 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
-        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Category not found!"));
+
+        return new CategoryDTO(entity);
     }
 }
