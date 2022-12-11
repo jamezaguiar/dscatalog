9bdacf92c9fd08d907aac6eefae4ba6a756b79d2
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2022-12-11 14:09:09 -0300

    refact params order

--- src/main/java/com/jamerson/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/jamerson/dscatalog/resources/CategoryResource.java
@@ -21,13 +21,12 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> findAll
-            (
-                    @RequestParam(value = "page", defaultValue = "0") Integer page,
-                    @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-                    @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-                    @RequestParam(value = "direction", defaultValue = "ASC") String direction
-            ) {
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
